531aa8186c58bf9404adca65c9c71b662289f723
mohammed zourob <fxfddfno@gmail.com>
2019-10-14 15:56:39 +0300

    clear code

--- js/index.js
+++ js/index.js
@@ -2,19 +2,21 @@ const $button = document.querySelector(".addGrid");
 const $contener = document.querySelector(".contener");
 const body = document.querySelector("window");
 
+// the main prompt
 function promptPop() {
-  let number = prompt("Please enter how many boxes you want!!");
-  number = compereByRegular(number);
-  while ($contener.firstChild) {
+  let number = prompt("Please enter how many boxes you want!!"); //the main prop
+  number = compereByRegular(number); //check number using regular expression
+  while ($contener.firstChild) { //clear counter
     $contener.removeChild($contener.firstChild);
   }
 
-  return number;
+  return number; // return number of dives
 }
 
+//add dives and there properties
 function addDivs() {
   const number = promptPop();
-  if (!number) {
+  if (!number) { //check if no num
     return promptPop();
   }
 
@@ -22,18 +24,19 @@ function addDivs() {
   const sqrtNumber = number;
   const numberEntered = number * number;
   const auto = "auto ";
+  //create div element
   const divInContener = document.createElement("DIV");
 
   let gridTemplateColumns = "";
-  for (let i = 0; i < sqrtNumber; i++) {
+  for (let i = 0; i < sqrtNumber; i++) { //number of auto to grid
     gridTemplateColumns += auto;
   }
 
   $contener.style.gridTemplateColumns = gridTemplateColumns;
+  // add dives and properties
   for (let i = 0; i < numberEntered; i++) {
     const divInContener = document.createElement("DIV");
     $contener.appendChild(divInContener);
-    // divInContener.textContent = `${i + 1}`;
     divInContener.style.background = "#eee";
     divInContener.style.color = "blue";
     divInContener.style.textAlign = "center";
@@ -44,33 +47,34 @@ function addDivs() {
   }
 }
 
+//generate random id
 function generateRandomColor(e) {
-  const lettersList = "123456789ABCDEF0";
+  const lettersList = "123456789ABCDEF0"; //list of random number and alph to create random color
   const arrayLetterList = lettersList.split("");
-  let color = "#";
+  let color = "#"; //color start with hash
   for (let i = 0; i < 6; i++) {
     color += arrayLetterList[Math.floor(Math.random() * 15)];
   }
-  this.style.background = color;
+  this.style.background = color; //add color
 }
 
+//delete color after some time
 function deleteColor(e) {
-  setTimeout(function () {
+  setTimeout(function () { //set time for clear color
     e.target.style.background = "#eee";
   }, 300);
 }
 
+//using regular expression to check input
 function compereByRegular(text) {
-  console.log(text);
   const Enter = text;
-  const reg = /[A-Z]/gi;
+  const reg = /[A-Z]/gi; //reg to check
+  //check if match or not
   const isMatch = Enter.match(reg);
-  console.log(isMatch);
   if (isMatch != null) {
     alert("You must enter number sorry");
-    console.log("You must enter number sorry");
     return promptPop();
   }
   return text;
 }
-$button.addEventListener("click", addDivs);
+$button.addEventListener("click", addDivs); //listen to click
